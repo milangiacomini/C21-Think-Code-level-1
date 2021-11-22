5e6eb84174938f0a0132e0f7eab321d9023df642
milangiacomini <milan.giaocmini@outlook.com>
2021-11-22 13:55:37 +0100

    opgelost

--- Opdracht2/opdracht2.py
+++ Opdracht2/opdracht2.py
@@ -1,9 +1,9 @@
 # syntax error
-print(Hello world)
+print("Hello world")
 
 # runtime error
 10 * (2/4)
 
 # semantic error
-name = 'alice'
-print("Hello", name)
+name='alice'
+print ("hello", name)

2007a85fae92e52df0f4d06eb51eb7cb2ee3df8b
milangiacomini <70368873+milangiacomini@users.noreply.github.com>
2021-11-23 13:59:59 +0100

    opdracht 5 gemaakt

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,23 @@
+# Prints out the name of a favorite food
+
+def favorite_food(name):
+    print("one of my favorite food is" , name)
+
+favorite_food("pasta")
+favorite_food("pizza")
+favorite_food("steak")
+
+
+
+  
+
+
+
+
+
+
+
+
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
